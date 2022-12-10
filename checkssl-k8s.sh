d9d2efc38c5363685d361e47dd0a9f283f868c9f
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2022-12-10 17:57:39 +0000

    Implemented check_ssl function

--- checkssl-k8s.sh
+++ checkssl-k8s.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
+check_ssl() {
+        echo $* | sed -e "s/ //g" | base64 -d | openssl x509 -noout -startdate -enddate -checkend 0 |   sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"
+}
+
 echo -e "\nChecking K8S certificates"
 echo "===="
 echo "Current system date: $(date)"
 echo "===="
-echo "SSL certificates expiration dates from the Kube-config file:" 
+echo "SSL certificates expiration dates from the Kube-config file:"
 echo "CA certificate:"
-grep "certificate-authority-data" ~/.kube/config | awk '{print $2}' |  base64 -d | openssl x509 -noout -startdate -enddate -checkend 0 |   sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"
+check_ssl $(grep "certificate-authority-data" ~/.kube/config | awk '{print $2}')
 echo -e "\nClient certificate:"
-grep "client-certificate-data" ~/.kube/config | awk '{print $2}' |  base64 -d | openssl x509 -noout -startdate -enddate -checkend 0 |   sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"
+check_ssl $(grep "client-certificate-data" ~/.kube/config | awk '{print $2}')
 echo "===="
 echo "SSL certificates expiration dates from the /etc/kubernetes/kubelet.conf file:"
 echo "CA certificate:"
-sudo grep "certificate-authority-data" /etc/kubernetes/kubelet.conf | awk '{print $2}' | base64 -d | openssl x509 -noout -startdate -enddate -checkend 0 |  sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"
+check_ssl $(sudo grep "certificate-authority-data" /etc/kubernetes/kubelet.conf | awk '{print $2}')
 echo -e "\nClient certificate:"
-sudo grep "client-certificate" /etc/kubernetes/kubelet.conf | awk '{print $2}' | base64 -d | openssl x509 -noout -startdate -enddate -checkend 0 | sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"
+check_ssl $(sudo grep "client-certificate" /etc/kubernetes/kubelet.conf | awk '{print $2}')
 echo "===="
 echo "SSL certificates expiration dates for the cluster certificates:"
-for cert in $(find /etc/kubernetes/pki/ -name "*.crt" | sort); do echo "$cert:"; openssl x509 -in $cert -noout -startdate -enddate -checkend 0|   sed "s/Certificate will expire/ (EXPIRED\!)/g" |  sed "s/Certificate will not expire/ (OK)/g"; echo;  done
+for cert in $(find /etc/kubernetes/pki/ -name "*.crt" | sort); do echo "$cert:"; check_ssl $(base64 $cert); echo; done
 echo "===="
 echo "SSL certificates expiration dates according to kubeadm:"
 sudo $(which kubeadm) certs check-expiration
